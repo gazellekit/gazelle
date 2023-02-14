f1af2a70799636252093daa9dd5e91b853f28cc2
James Bayley <36523314+jamesbayley@users.noreply.github.com>
2023-02-14 21:38:20 +0000

    Updated Calcpad.ETABS project.

--- src/Calcpad.ETABS/Types.fs
+++ src/Calcpad.ETABS/Types.fs
@@ -1,10 +1,10 @@
 namespace Calcpad.ETABS
 
-type StoreyRenderProps = 
-    { Name: string 
-      Height: float 
-      IsMaster: bool 
-      MasterRefName: string 
-      IsSplicedAbove: bool 
-      SpliceHeight: float 
-      Color: int }
+// type StoreyRenderProps = 
+//     { Name: string 
+//       Height: float 
+//       IsMaster: bool 
+//       MasterRefName: string 
+//       IsSplicedAbove: bool 
+//       SpliceHeight: float 
+//       Color: int }
