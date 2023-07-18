495a475338ca11b05f096b1507c94c67f8f343ee
James Bayley <36523314+jamesbayley@users.noreply.github.com>
2023-07-18 14:17:26 +0000

    Bundle and minify library code

--- projects/core/esbuild.config.mjs
+++ projects/core/esbuild.config.mjs
@@ -1,23 +1,23 @@
 import * as esbuild from 'esbuild';
 
-// await esbuild.build({
-//   entryPoints: ['./Interop/JS.fs.js'],
-//   bundle: true,
-//   minify: true,
-//   sourcemap: false,
-//   target: 'es2022',
-//   outfile: 'fable_build/calcpad.studio.min.js',
-//   globalName: 'Calcpad.Studio',
-//   format: 'iife'
-// });
+await esbuild.build({
+  entryPoints: ['./build/main.js'],
+  bundle: true,
+  minify: true,
+  sourcemap: false,
+  target: 'es2022',
+  outfile: 'build/calcpad.min.js',
+  globalName: 'Calcpad',
+  format: 'iife'
+});
 
-// await esbuild.build({
-//   entryPoints: ['./Interop/JS.fs.js'],
-//   bundle: true,
-//   minify: false,
-//   sourcemap: false,
-//   target: 'es2022',
-//   outfile: 'fable_build/calcpad.studio.js',
-//   globalName: 'Calcpad.Studio',
-//   format: 'iife'
-// });
+await esbuild.build({
+  entryPoints: ['./build/main.js'],
+  bundle: true,
+  minify: false,
+  sourcemap: false,
+  target: 'es2022',
+  outfile: 'build/calcpad.js',
+  globalName: 'Calcpad',
+  format: 'iife'
+});
