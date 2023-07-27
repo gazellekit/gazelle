039940f82ccd3715dc34fc3e6a1a27c88c151ec6
James Bayley <36523314+jamesbayley@users.noreply.github.com>
2023-07-27 11:54:50 +0000

    Configure JS bundling, incl. typings

---
+++ projects/fortress/src/esbuild.config.mjs
@@ -0,0 +1,24 @@
+import * as esbuild from 'esbuild';
+
+await esbuild.build({
+  entryPoints: ['./Fortress.fs.ts'],
+  outfile: './build/js/fortress.js',
+  platform: 'node',
+  format: 'esm',
+  target: 'es2022',
+  sourcemap: 'linked',
+  bundle: true,
+  minify: false
+});
+
+await esbuild.build({
+  entryPoints: ['./Fortress.fs.ts'],
+  outfile: './build/js/fortress.min.js',
+  platform: 'browser',
+  format: 'iife',
+  globalName: 'Fortress',
+  target: 'es2022',
+  sourcemap: 'linked',
+  bundle: true,
+  minify: true
+});
