62881bab855adbc20cd2c9dc0a0e36186b6b1fad
James Bayley <36523314+jamesbayley@users.noreply.github.com>
2023-07-27 11:56:25 +0000

    Test Fortress API from web project

--- projects/web/src/main.js
+++ projects/web/src/main.js
@@ -1,4 +1,7 @@
+import { sayHello } from '@calcpad.studio/fortress';
 import { Rectangle } from '@calcpad.studio/structures';
 
 const rect = new Rectangle(10, 20);
 console.log(rect.secondMomentOfArea('X-X'));
+
+sayHello();
