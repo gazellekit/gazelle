ecd1a249291d37a76b516f9078f21c6d6e4b762a
James Bayley <36523314+jamesbayley@users.noreply.github.com>
2023-07-28 12:30:12 +0000

    Add Calcpad.Studio.Interop.CSI project

---
+++ projects/Calcpad.Studio.Interop.CSI/ETABS/Types.fs
@@ -0,0 +1,10 @@
+namespace Calcpad.Studio.Interop.CSI
+
+type StoreyRenderProps = 
+    { Name: string 
+      Height: float 
+      IsMaster: bool 
+      MasterRefName: string 
+      IsSplicedAbove: bool 
+      SpliceHeight: float 
+      Color: int }
