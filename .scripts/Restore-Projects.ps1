d626269432e6875a15634b38c6f370a7352cfd0b
James Bayley <36523314+jamesbayley@users.noreply.github.com>
2023-07-28 16:37:25 +0100

    Remove Fable Dependency & Configuration. (#71)
    * Remove C# debug assets
    * Remove Fable dependency and configuration

--- .scripts/Restore-Projects.ps1
+++ .scripts/Restore-Projects.ps1
@@ -3,7 +3,6 @@ Set-Location "$PSScriptRoot/.."
 $DirectoryCommands = @(
   @{ RelativePath = "."; Command = "npm install -g npm" }
   @{ RelativePath = "./projects"; Command = "dotnet tool restore" }
-  @{ RelativePath = "./projects/Calcpad.Studio.Core"; Command = "npm install" }
 ) 
 
 $DirectoryCommands | ForEach-Object { 
