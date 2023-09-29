3cf7a4bc8626f6c887be9e00ac209495cbdf997f
James Bayley <36523314+jamesbayley@users.noreply.github.com>
2023-09-29 15:00:42 +0000

    Create bluebook package

---
+++ pkg/bluebook/doc.go
@@ -0,0 +1,8 @@
+// As Structural Engineers adopt digital workflows, the availability of data
+// in consumable formats becomes increasingly important. Structural design data
+// is often captured within Excel spreadsheets, books, tables and figures, which
+// is inconvenient from a software perspective. Moreover, mapping data from one
+// format to another can be time-consuming and a frequent source of error.
+// Bluebook encapsulates the Steel for Life steel section data as a library
+// with a stable and convenient API.
+package bluebook
