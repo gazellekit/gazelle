a5647311fc8c1454105377b7a9fb20b04c87d2d2
James Bayley <36523314+jamesbayley@users.noreply.github.com>
2023-10-01 20:29:41 +0000

    Add doc.go

--- pkg/bluebook/doc.go
+++ pkg/bluebook/doc.go
@@ -1,8 +1,5 @@
-// As Structural Engineers adopt digital workflows, the availability of data
-// in consumable formats becomes increasingly important. Structural design data
-// is often captured within Excel spreadsheets, books, tables and figures, which
-// is inconvenient from a software perspective. Moreover, mapping data from one
-// format to another can be time-consuming and a frequent source of error.
-// Bluebook encapsulates the Steel for Life steel section data as a library
-// with a stable and convenient API.
+// A library to encapsulate the 'Bluebook' steel section data.
+//
+// The original .xlsx data files were sourced from:
+// https://www.steelforlifebluebook.co.uk and transformed.
 package bluebook
