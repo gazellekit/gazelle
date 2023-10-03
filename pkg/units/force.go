77308d063d2c27e522b7e61211a23b18d01fa3cb
James Bayley <36523314+jamesbayley@users.noreply.github.com>
2023-10-03 15:16:46 +0000

    Simplify unit system

--- pkg/units/force.go
+++ pkg/units/force.go
@@ -2,40 +2,27 @@ package units
 
 import (
 	"fmt"
-
-	"golang.org/x/exp/constraints"
 )
 
-// The metric unit of force.
-type Newton[T constraints.Float] struct {
-	Value T
+type Newton float64
+type Kilonewton float64
+
+type Force interface {
+	Newton | Kilonewton
 }
 
-// The metric unit of force.
-type Kilonewton[T constraints.Float] struct {
-	Value T
+func (N Newton) ToKilonewton() Kilonewton {
+	return Kilonewton(N / 1000.0)
 }
 
-// A union type that represents the
-// combined set of units of force.
-type Force[T constraints.Float] interface {
-	Newton[T] | Kilonewton[T]
+func (N Newton) String() string {
+	return fmt.Sprintf("%f Newton(s)", N)
 }
 
-// Convert metric Newtons to Kilonewtons.
-func (N Newton[T]) ToKilonewton() Kilonewton[T] {
-	return Kilonewton[T]{Value: N.Value / 1000.0}
+func (kN Kilonewton) ToNewton() Newton {
+	return Newton(kN * 1000.0)
 }
 
-// Convert metric Kilonewtons to Newtons.
-func (kN Kilonewton[T]) ToNewton() Newton[T] {
-	return Newton[T]{Value: kN.Value * 1000.0}
-}
-
-func (N Newton[T]) String() string {
-	return fmt.Sprintf("%f Newton(s)", N.Value)
-}
-
-func (kN Kilonewton[T]) String() string {
-	return fmt.Sprintf("%f Kilonewton(s)", kN.Value)
+func (kN Kilonewton) String() string {
+	return fmt.Sprintf("%f Kilonewton(s)", kN)
 }
