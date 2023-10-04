3d7a284ea6b36f340349d15a928aecd1eafa621f
James Bayley <36523314+jamesbayley@users.noreply.github.com>
2023-10-04 21:45:56 +0000

    Update package docstring

--- pkg/bluebook/doc.go
+++ pkg/bluebook/doc.go
@@ -1,5 +1,10 @@
-// A library to encapsulate the 'Bluebook' steel section data.
-//
-// The original .xlsx data files were sourced from:
-// https://www.steelforlifebluebook.co.uk and transformed.
+// As Structural Engineers adopt digital workflows, the availability
+// of data in consumable formats becomes increasingly important.
+// Structural design data is often captured within Excel spreadsheets,
+// books, tables and figures, which is inconvenient from a software
+// perspective. Moreover, mapping data from one format to another can
+// be time-consuming and a frequent source of error. The bluebook
+// package encapsulates the Steel for Life steel section data as a
+// library with a stable and convenient API. The original .xlsx data
+// files were sourced from: https://www.steelforlifebluebook.co.uk.
 package bluebook
