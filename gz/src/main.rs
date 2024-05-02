a35f29a1a608607b69c42258451b8ec777ef10d1
James Bayley <36523314+jamesbayley@users.noreply.github.com>
2024-05-02 19:19:53 +0100

    Update project structure (#126)

--- gz/src/main.rs
+++
@@ -1,4 +0,0 @@
-fn main() {
-    println!("Welcome to Gazelle! 🦌");
-    println!("A faster, safer engine for civil engineering design.");
-}
---
+++ src/main.rs
@@ -0,0 +1,4 @@
+fn main() {
+    println!("Welcome to Gazelle! 🦌");
+    println!("A fast engine for civil engineering design.");
+}
