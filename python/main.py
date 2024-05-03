148b6f85341b20753933bd5243e9411772c98fb2
James Bayley <36523314+jamesbayley@users.noreply.github.com>
2024-05-03 20:19:00 +0000

    Update project structure to fix import error

---
+++ python/main.py
@@ -0,0 +1,11 @@
+import gazelle as gz
+
+from .steel import SectionCategory, SteelFabricator, UniversalBeam
+
+print("Welcome to Gazelle! 🦌")
+print(f"The answer is: {gz.sum_as_string(10, 32)}")
+
+sections = SteelFabricator[UniversalBeam].make_all_sections(SectionCategory.UB)
+
+for section in sections:
+  print(section)
