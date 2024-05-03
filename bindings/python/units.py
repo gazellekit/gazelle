dc9048fb6c78ea3798ad6ab143e0297419f5cf32
James Bayley <36523314+jamesbayley@users.noreply.github.com>
2024-05-03 20:21:47 +0100

    Update directory structure (#136)

--- bindings/python/units.py
+++ bindings/python/units.py
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
-# Gazelle: A fast engine for civil engineering design.
+# Gazelle: A fast, open-source engine for civil engineers.
 # Copyright (C) 2024 James S. Bayley
 
 
