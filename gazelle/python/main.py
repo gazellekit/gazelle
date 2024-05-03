39c0184189dd7400bebacb9d897477f66c600a28
James Bayley <36523314+jamesbayley@users.noreply.github.com>
2024-05-03 15:29:06 +0100

    Add `steel.py` and `units.py` files (#133)

--- gazelle/python/main.py
+++ gazelle/python/main.py
@@ -1,4 +1,11 @@
 import gazelle as gz
+from python.steel import SteelFabricator, SectionCategory, UniversalBeam
 
 print("Welcome to Gazelle! 🦌")
 print(f'The answer is: {gz.sum_as_string(10, 32)}')
+
+sections = SteelFabricator[UniversalBeam] \
+  .make_all_sections(SectionCategory.UB)
+
+for section in sections:
+  print(section)
