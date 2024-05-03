833c2c79fab00d9c72e398f6528762aadc76f4a8
James Bayley <36523314+jamesbayley@users.noreply.github.com>
2024-05-03 15:05:16 +0100

    Adopt 'monorepo' directory structure (#132)

--- bindings/python/main.py
+++
@@ -1,4 +0,0 @@
-from gazellekit import sum_as_string
-
-print("Welcome to Gazelle! 🦌")
-print(sum_as_string(10, 30))
---
+++ gazelle/python/main.py
@@ -0,0 +1,4 @@
+import gazelle as gz
+
+print("Welcome to Gazelle! 🦌")
+print(f'The answer is: {gz.sum_as_string(10, 32)}')
